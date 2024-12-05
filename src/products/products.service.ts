b1ddf7991921e2c5a70489c29e7de61a64dd6584
dev_hrios <hrios@tiprotec.com>
2024-12-05 16:50:05 -0600

    Update metodo patch implementando queryRunner 1/2

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product, ProductImage } from './entities';
@@ -17,7 +17,10 @@ export class ProductsService {
     private readonly productRepository: Repository<Product>,
 
     @InjectRepository(ProductImage)
-    private readonly productImageRepository: Repository<ProductImage>
+    private readonly productImageRepository: Repository<ProductImage>,
+
+    private readonly dataSource: DataSource,
+
   ) { }
 
   async create(createProductDto: CreateProductDto) {
@@ -102,13 +105,15 @@ export class ProductsService {
 
   async update(id: string, updateProductDto: UpdateProductDto) {
 
-    const product = await this.productRepository.preload({
-      id: id,
-      ...updateProductDto,
-      images: [],
-    })
+    const { images, ...toUpdate } = updateProductDto;
+
+    const product = await this.productRepository.preload({id, ...toUpdate,})
 
     if (!product) throw new NotFoundException(`Product with id: ${id} is not found`)
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    
+
 
     try {
 
