92ccc3662a8c2146b7cb66bfbb6af224dc873782
dev_hrios <hrios@tiprotec.com>
2024-12-11 05:40:34 -0600

    Guardar archivos en repo

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -2,22 +2,25 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseFilePipe, Uploa
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) { }
 
-  // servicio carga archivo con Express y agregando @types/multer
+  // Idealmente guardar archivos en un bucket independiente al proyecto
+  // Aqui se muestra como guardarlos en este repo (archivos sin relevancia o proyectos muy pequeños)
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
-    fileFilter: fileFilter
+    fileFilter: fileFilter,
+    // limits: { fileSize: 1000}
+    storage: diskStorage({
+      destination: './static/uploads'
+    })
   }))
   uploadProductImage(
     @UploadedFile() file: Express.Multer.File
   ) {
-
-    // `fileInController` viene vacio si no se validó en true por alguna de las condiciones del archivo fileFilter.helper
-    // console.log({ fileInController: file })
 
     if (!file) {
       throw new BadRequestException('File must be a supported image format')
@@ -28,5 +31,4 @@ export class FilesController {
     };
   }
 
-  // servicio carga archivo con Pipe del nestjs/common
 }
