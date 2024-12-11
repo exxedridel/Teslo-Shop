9f74fefe3049058311fdc8d41b2c5664facae094
dev_hrios <hrios@tiprotec.com>
2024-12-11 15:07:04 -0600

    Add: async module to process env variables to sign JWTs

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
@@ -9,7 +12,32 @@ import { User } from './entities/user.entity';
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User]),
+
+    PassportModule.register({ defauktStrategy: 'jwt' }),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // console.log('JWT Secret: ', configService.get('JWT_SECRET'))
+        // console.log('JWT SECRET: ', process.env.JWT_SECRET)
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h'
+          }
+        }
+      }
+    })
+
+    // esta manera no es syncrona, si no esta configurada la variable de entorno puede fallar, mejor se hace como arriba
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '2h'
+    //   }
+    // })
   ],
   exports: [TypeOrmModule]
 })
